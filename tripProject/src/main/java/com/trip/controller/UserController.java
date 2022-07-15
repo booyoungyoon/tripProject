f4bbe80dc24a92ebd1b78fb16363947da757b164
IceChoco <95326550+Hotmintchoco@users.noreply.github.com>
2022-07-15 22:52:07 +0900

    DB연결 중단
    DB연결 구문 주석처리

--- tripProject/src/main/java/com/trip/controller/UserController.java
+++ tripProject/src/main/java/com/trip/controller/UserController.java
@@ -9,17 +9,17 @@ import com.trip.mapper.UserMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
-@Controller
-@Log4j
-@AllArgsConstructor
-@RequestMapping("/users/*")
-public class UserController {
-	private UserMapper mapper;
-	
-	@RequestMapping("list")
-	public void list(Model model) {
-		log.info("list");
-		model.addAttribute("list", mapper.getList());
-	}
-	
-}
+//@Controller
+//@Log4j
+//@AllArgsConstructor
+//@RequestMapping("/users/*")
+//public class UserController {
+//	private UserMapper mapper;
+//	
+//	@RequestMapping("list")
+//	public void list(Model model) {
+//		log.info("list");
+//		model.addAttribute("list", mapper.getList());
+//	}
+//	
+//}

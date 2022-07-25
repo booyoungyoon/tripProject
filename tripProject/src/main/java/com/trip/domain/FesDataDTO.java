a3fff480c307d7058cc8450795c48b9cc2201ddd
IceChoco <95326550+Hotmintchoco@users.noreply.github.com>
2022-07-25 18:08:06 +0900

    festivaldata 테이블 변경
    festivaldata 테이블 정보 수정

--- tripProject/src/main/java/com/trip/domain/FesDataDTO.java
+++ tripProject/src/main/java/com/trip/domain/FesDataDTO.java
@@ -5,13 +5,13 @@ import lombok.Data;
 /*
 create table festivaldata (
     num number(5) not null primary key,
-    title varchar2(50),
-    address varchar2(100),
-    firstimg varchar2(500),
+    title varchar2(100),
+    address varchar2(200),
+    firstimg varchar2(2000),
     startdate varchar2(8),
     enddate varchar2(8),
-    mapx varchar2(50),
-    mapy varchar2(50),
+    mapx varchar2(100),
+    mapy varchar2(100),
     mlevel number(5)
 ); 
  */
